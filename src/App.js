fa1c2f2ba3ac5137c0fb4f8b7e91c35d2e36aacc
Gowtham Behara <gothsiddu@gmail.com>
2019-09-28 11:39:59 +0530

    Enclose body in Router with seperate Routes for crete, edit and view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="container">
-      <h2>TodoList MERN Stack</h2>
-    </div>
+    <Router>
+      <div className="container">
+        <h2>TodoList MERN Stack</h2>
+      </div>
+      <Route path="/" exact component={TodoList} />
+      <Route path="/edit/:id" exact component={EditTodo} />
+      <Route path="/create" exact component={CreateTodo} />
+    </Router>
   );
 }
 
