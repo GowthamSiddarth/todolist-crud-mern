3e1848d97cdec173b7dd3ec4a5426be872cc6fca
Gowtham Behara <gothsiddu@gmail.com>
2019-09-28 16:18:27 +0530

    Retrieve todos list from backend and render

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+function Todo(props) {
+    return (
+        <tr>
+            <td>{props.todo.description}</td>
+            <td>{props.todo.responsible}</td>
+            <td>{props.todo.priority}</td>
+            <td>
+                <Link to={"update/" + props.todo._id}>Update</Link>
+            </td>
+        </tr>
+    );
+}
 
 class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/api/todo/list')
+            .then(res => {
+                this.setState({ todos: res.data });
+            })
+            .catch(err => console.log(err));
+    }
+
+    getTodoList() {
+        return this.state.todos.map((todo, idx) => {
+            return <Todo todo={todo} key={idx} />
+        });
+    }
+
     render() {
         return (
             <div>
-                <p>Todo List Items will appear here</p>
+                <h3>Todos List</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Responsible</th>
+                            <th>Priority</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.getTodoList()}
+                    </tbody>
+                </table>
             </div>
         );
     }
