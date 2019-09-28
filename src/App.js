afb7622ff816fe1f433929973edef2458007b14d
Gowtham Behara <gothsiddu@gmail.com>
2019-09-28 15:19:48 +0530

    Rearrange code to correct components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoList from './components/todo-list';
 import EditTodo from './components/edit-todo';
 import CreateTodo from './components/create-todo';
+import Welcome from './components/welcome';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
-                <Link to="/" className="nav-link">Todos</Link>
+                <Link to="/list" className="nav-link">Todos</Link>
               </li>
               <li className="navbar-item">
                 <Link to="/create" className="nav-link">Create Todo</Link>
@@ -26,7 +27,8 @@ function App() {
           </div>
         </nav>
         <br />
-        <Route path="/" exact component={TodoList} />
+        <Route path="/" exact component={Welcome} />
+        <Route path="/list" exact component={TodoList} />
         <Route path="/edit/:id" exact component={EditTodo} />
         <Route path="/create" exact component={CreateTodo} />
       </div>
--- src/components/create-todo.js
+++ src/components/create-todo.js
@@ -1,11 +1,120 @@
 import React, { Component } from "react";
 
 class CreateTodo extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: '',
+            responsible: '',
+            priority: '',
+            completed: false
+        };
+
+        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+        this.handleResponsibleChange = this.handleResponsibleChange.bind(this);
+        this.handlePriorityChange = this.handlePriorityChange.bind(this);
+        this.handleCompletedChange = this.handleCompletedChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    handleDescriptionChange(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    handleResponsibleChange(e) {
+        this.setState({
+            responsible: e.target.value
+        })
+    }
+
+    handlePriorityChange(e) {
+        this.setState({
+            priority: e.target.value
+        })
+    }
+
+    handleCompletedChange(e) {
+        this.setState({
+            completed: e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        this.state({
+            description: '',
+            responsible: '',
+            priority: '',
+            completed: false
+        });
+    }
+
     render() {
         return (
-            <div>
-                <p>Welcome to Create Todo Component</p>
-            </div>
+            <div style={{ marginTop: 10 }}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.description}
+                            onChange={this.handleDescriptionChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.responsible}
+                            onChange={this.handleResponsibleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityLow"
+                                value="Low"
+                                checked={this.state.priority === 'Low'}
+                                onChange={this.handlePriorityChange}
+                            />
+                            <label className="form-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityMedium"
+                                value="Medium"
+                                checked={this.state.priority === 'Medium'}
+                                onChange={this.handlePriorityChange}
+                            />
+                            <label className="form-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input"
+                                type="radio"
+                                name="priorityOptions"
+                                id="priorityHigh"
+                                value="High"
+                                checked={this.state.priority === 'High'}
+                                onChange={this.handlePriorityChange}
+                            />
+                            <label className="form-check-label">High</label>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div >
         );
     }
 }
