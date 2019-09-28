44bd8c531b46ab80765007cf90b89eedc6f1ccc9
Gowtham Behara <gothsiddu@gmail.com>
2019-09-28 12:20:46 +0530

    Create a navbar with links to Todos and Create Todo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import TodoList from './components/todo-list';
 import EditTodo from './components/edit-todo';
 import CreateTodo from './components/create-todo';
@@ -10,11 +12,23 @@ function App() {
   return (
     <Router>
       <div className="container">
-        <h2>TodoList MERN Stack</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to="/" className="navbar-brand">TodoList App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={TodoList} />
+        <Route path="/edit/:id" exact component={EditTodo} />
+        <Route path="/create" exact component={CreateTodo} />
       </div>
-      <Route path="/" exact component={TodoList} />
-      <Route path="/edit/:id" exact component={EditTodo} />
-      <Route path="/create" exact component={CreateTodo} />
     </Router>
   );
 }
